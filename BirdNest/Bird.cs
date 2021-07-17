f3c7527d149eeb6722b6098ce78d842b375ee202
oshan.m <oshan.m@yahoo.com>
2021-07-17 18:38:46 +1000

    Add project files.

---
+++ BirdNest/Bird.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BirdNest
+{
+    class Bird
+    {
+        public static Random Randomizer = new Random();
+        public virtual Egg[] LayEggs(int numberOfEggs)
+        {
+            Console.Error.WriteLine("Bird.LayEggs should never get called");
+            return new Egg[0];
+        }
+    }
+}
