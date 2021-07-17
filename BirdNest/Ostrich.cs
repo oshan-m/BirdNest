c304152979bf9dc80901b0821aca45f494165d51
oshan.m <oshan.m@yahoo.com>
2021-07-17 19:53:46 +1000

    Inherihance - final

---
+++ BirdNest/Ostrich.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BirdNest
+{
+    class Ostrich : Bird
+    {
+        public override Egg[] LayEggs(int numberOfEggs)
+        {
+            Egg[] ostrichEgg = new Egg[numberOfEggs];
+            for (int i = 0; i < numberOfEggs; i++)
+            {
+                ostrichEgg[i] = new Egg(Bird.Randomizer.NextDouble() + 12, "speckled"); 
+            }
+
+            return ostrichEgg;
+        }
+    }
+}
---
+++ BirdNest/Pigeon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BirdNest
+{
+    class Pigeon : Bird
+    {
+        public override Egg[] LayEggs(int numberOfEggs)
+        {
+            Egg[] pigeonEgg = new Egg[numberOfEggs];
+            for (int i = 0; i < numberOfEggs; i++) {
+                if (Bird.Randomizer.Next(4) == 0)
+                {
+                    pigeonEgg[i] = new BrokenEgg( "white");
+                }
+                else
+                {
+                    pigeonEgg[i] = new Egg(Bird.Randomizer.NextDouble() * 2 + 1, "white"); 
+                }
+            }
+
+            return pigeonEgg;
+        }
+    }
+}
